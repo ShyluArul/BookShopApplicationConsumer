2c47c9718b8ca30e08b5f3f11652330774cb8f4c
ShyluArul <56394622+ShyluArul@users.noreply.github.com>
2020-05-09 06:47:14 -0700

    Update BookConsumerController.java

--- bookshopapplicationconsumerpact/src/main/java/com/bookshopapp/controller/BookConsumerController.java
+++ bookshopapplicationconsumerpact/src/main/java/com/bookshopapp/controller/BookConsumerController.java
@@ -1,16 +1,13 @@
 package com.bookshopapp.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.bookshopapp.entities.Book;
 import com.bookshopapp.service.BookConsumerService;
 
 @RestController
-
 public class BookConsumerController {
 	@Autowired
 	BookConsumerService bookConsumerService;
@@ -22,9 +19,7 @@ public class BookConsumerController {
 
 	@GetMapping("book/{id}")
 	public ResponseEntity<Book> getBook(@PathVariable("id") Long id) {
-
 		return bookConsumerService.getBook(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
 
 	}
-
 }
